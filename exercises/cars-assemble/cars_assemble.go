8cab78e37a12a08cd981a3778b1f318093ffbe20
hatai <20625256+hatai@users.noreply.github.com>
2025-09-11 02:57:23 +0900

    feat: 車の組立エクササイズの初期実装とテストの追加

---
+++ exercises/cars-assemble/cars_assemble.go
@@ -0,0 +1,25 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * successRate / 100
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	var cost int = 10000
+	var discountedCost int = 9500
+	// 1 の位
+	var remaining int = carsCount % 10
+	// 1 の位を除いた数
+	var groups int = carsCount - remaining
+
+	return uint(remaining * cost) + uint(groups * discountedCost)
+}
